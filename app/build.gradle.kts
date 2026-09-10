9b353efedebec9fb98575656874f1e661d04db9b
LuMarans30 <andreamarano2004@gmail.com>
2026-09-11 00:13:32 +0200

    chore: bump version to 0.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.lumarans30.hexamesh"
         minSdk = 34
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.2.0"
+        versionCode = 2
+        versionName = "0.3.0"
         ndk {
             abiFilters.add("arm64-v8a")
         }
