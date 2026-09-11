83cc4eb144dc93fc039b3f073c30e6b23d9cdcbb
LuMarans30 <andreamarano2004@gmail.com>
2026-09-11 11:53:49 +0200

    chore: bump version to 0.6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.lumarans30.hexamesh"
         minSdk = 34
         targetSdk = 36
-        versionCode = 4
-        versionName = "0.5.0"
+        versionCode = 5
+        versionName = "0.6.0"
         ndk {
             abiFilters.add("arm64-v8a")
         }
